c1bd009b1cb2ca29c24a1229513e615ad73c5d08
Stephan H. Wissel <stephan@wissel.net>
2017-03-14 09:18:28 +0800

    Updated license information

--- plugin/src/com/ibm/issc/domino/oooservice/WinkServlet.java
+++ plugin/src/com/ibm/issc/domino/oooservice/WinkServlet.java
@@ -1,10 +1,22 @@
 /** ========================================================================= *
- * Copyright (C)  2016 IBM Corporation ( http://www.ibm.com/ )                *
+ * Copyright (C)  2017 IBM Corporation ( http://www.ibm.com/ )                *
  *                            All rights reserved.                            *
  *                                                                            *
  *  @author     Stephan H. Wissel (stw) <st.wissel@sg.ibm.com>                *
  *                                       @notessensei                         *
  * @version     1.0                                                           *
+ * ========================================================================== *
+ *                                                                            *
+ * Licensed under the  Apache License, Version 2.0  (the "License").  You may *
+ * not use this file except in compliance with the License.  You may obtain a *
+ * copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>.       *
+ *                                                                            *
+ * Unless  required  by applicable  law or  agreed  to  in writing,  software *
+ * distributed under the License is distributed on an  "AS IS" BASIS, WITHOUT *
+ * WARRANTIES OR  CONDITIONS OF ANY KIND, either express or implied.  See the *
+ * License for the  specific language  governing permissions  and limitations *
+ * under the License.                                                         *
+ *                                                                            *
  * ========================================================================== *
  * com.ibm.verse.domino.servlet com.ibm.verse.domino.servlet WinkServlet.java
  */
@@ -14,7 +26,7 @@ import com.ibm.domino.services.AbstractRestServlet;
 
 /**
  * Placeholder, doesn't need additional content
- * See: ttps://frostillic.us/blog/posts/1CD1740C77996C8B85257F10006FEB21
+ * See: https://frostillic.us/blog/posts/1CD1740C77996C8B85257F10006FEB21
  *
  * @author stw
  */
